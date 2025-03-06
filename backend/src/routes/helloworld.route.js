1e8f987b8d884dd6832edd81af865bf4dcb6e27d
kng1259 <khoinguyenanlac1@gmail.com>
2025-03-06 08:12:28 +0700

    change docs location

--- backend/src/routes/helloworld.route.js
+++ backend/src/routes/helloworld.route.js
@@ -5,6 +5,31 @@ import { authMiddleware } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * tags:
+ *   name: HelloWorld
+ *   description: Sample endpoint for reference
+ */
+
+/**
+ * @swagger
+ * /api/v1:
+ *   get:
+ *     summary: Returns a hello world message
+ *     tags: [HelloWorld]
+ *     responses:
+ *       200:
+ *         description: Hello world message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello World
+ */
 router.get('/', authMiddleware.isAuthorized, asyncHandler(helloworldCtrl.helloworld))
 
 export default router
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,36 +1,9 @@
 import express from 'express'
 import helloworld from './helloworld.route.js'
 import { userRoute } from './user.route.js'
-
 const router = express.Router()
 
-/**
- * @swagger
- * tags:
- *   name: HelloWorld
- *   description: Sample endpoint for reference
- */
-
-/**
- * @swagger
- * /api/v1:
- *   get:
- *     summary: Returns a hello world message
- *     tags: [HelloWorld]
- *     responses:
- *       200:
- *         description: Hello world message
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: Hello World
- */
 router.use('/', helloworld)
 router.use('/user', userRoute)
-
 
 export default router
