a2dd66c4d1e63dab02e672db89f07dda25acca3e
NguyenBk22 <150934815+NguyenBk22@users.noreply.github.com>
2025-03-20 20:44:26 +0700

    7 first collum
    change

--- backend/src/controllers/data.controller.js
+++ backend/src/controllers/data.controller.js
@@ -2,13 +2,13 @@ import dataService from '../services/data.service.js';
 
 export const getChartData = async (req, res, next) => {
     try {
-        const { range, areaId } = req.query; // '7d' or '7h', optional areaId filter
+        const { areaId } = req.query; // '7d' or '7h', optional areaId filter
 
-        if (!range || (range !== '7d' /*&& range !== '7h'*/)) {
-            return res.status(400).json({ success: false, error: "Invalid range." });
+        if (!areaId){
+            return res.status(400).json({ success: false, error: "Invalid areaId."});
         }
 
-        const data = await dataService.getChartData(range, areaId);
+        const data = await dataService.getChartData(areaId);
         res.json({ success: true, data });
     } catch (error) {
         next(error);
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,10 +4,12 @@ import { userRoute } from './user.route.js'
 import { farmRoute } from './farm.route.js'
 import { areaRoute } from './area.route.js'
 import { recordRoute } from './record.route.js'
+import dataRoute from './data.route.js'
 
 const router = express.Router()
 
 router.use('/', helloworld)
+router.use('/data', dataRoute)
 router.use('/user', userRoute)
 router.use('/farms', farmRoute)
 router.use('/areas', areaRoute)
