188b2f9fcf8d59910f512c7aee2e6e1c6cdf24b4
kng1259 <kngondajob@gmail.com>
2025-03-21 18:53:26 +0700

    autogenerate records

--- backend/prisma/records.js
+++ backend/prisma/records.js
@@ -1,4 +1,5 @@
-import fs, { write } from 'fs'
+import fs from 'fs';
+import { v4 as uuid4 } from 'uuid';
 
 /**
  * Write an array of objects to a JSON file.
@@ -7,34 +8,50 @@ import fs, { write } from 'fs'
  * @param {string} outputPath - Path to the output JSON file
  */
 function writeDataToJsonFile(data, outputPath) {
-  try {
-    // Convert the data to a formatted JSON string
-    const jsonData = JSON.stringify(data, null, 2)
+    try {
+        // Convert the data to a formatted JSON string
+        const jsonData = JSON.stringify(data, null, 2);
 
-    // Write to file
-    fs.writeFileSync(outputPath, jsonData)
+        // Write to file
+        fs.writeFileSync(outputPath, jsonData);
 
-    console.log(`Data successfully written to ${outputPath}`)
-  } catch (error) {
-    console.error('Error writing to JSON file:', error)
-  }
+        console.log(`Data successfully written to ${outputPath}`);
+    } catch (error) {
+        console.error('Error writing to JSON file:', error);
+    }
+}
+
+function getRandomInRange() {
+    return 0.7 + Math.random() * 0.6;
 }
 
 function generateData(amount) {
-  let data = []
-  const timestamp = new Date()
-  for (var i = 0; i < amount; i++) {
-    const recordTimestamp = new Date(timestamp.getTime() + i * 5000)
-    data.push({
-      timestamp: recordTimestamp,
-      light: Math.random() * 25,
-      temperature: Math.random() * 25,
-      humidity: Math.random() * 25,
-      soilMoisture: Math.random() * 25,
-      areaId: 'area1234-5678-90ab-cdef-1234567890ab',
-    })
-  }
-  return data
+    let data = [];
+    const timestamp = new Date(Date.now() - amount * 60000);
+    for (var i = 0; i < amount; i++) {
+        const recordTimestamp = new Date(timestamp.getTime() + i * 60000);
+        data.push({
+            id: `rec${i}`,
+            timestamp: recordTimestamp,
+            light: getRandomInRange() * 500,
+            temperature: getRandomInRange() * 25,
+            humidity: getRandomInRange() * 60,
+            soilMoisture: getRandomInRange() * 40,
+            areaId: 'area1',
+        });
+        data.push({
+            id: `rec${i}`,
+            timestamp: recordTimestamp,
+            light: getRandomInRange() * 500,
+            temperature: getRandomInRange() * 25,
+            humidity: getRandomInRange() * 60,
+            soilMoisture: getRandomInRange() * 40,
+            areaId: 'area2',
+        });
+    }
+    return data;
 }
 
-writeDataToJsonFile(generateData(100), 'prisma/data/Record.json')
+export function seedRecordData() {
+    writeDataToJsonFile(generateData(120), 'prisma/data/Record.json');
+}
