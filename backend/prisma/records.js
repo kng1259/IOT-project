33a5ea57120b282c222073fe9a56fe302b53a319
kng1259 <kngondajob@gmail.com>
2025-03-24 11:39:52 +0700

    Update Jenkinsfile

--- backend/prisma/records.js
+++ backend/prisma/records.js
@@ -52,5 +52,6 @@ function generateData(amount) {
 }
 
 export function seedRecordData() {
-    writeDataToJsonFile(generateData(96), 'prisma/data/Record.json');
+    writeDataToJsonFile(generateData(96), './data/Record.json');
+    console.log('Record data seeded');
 }
