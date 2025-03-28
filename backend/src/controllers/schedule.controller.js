3dcc3f638fcee2a384882adb3f67b53c9ea76bc9
NguyenBk22 <150934815+NguyenBk22@users.noreply.github.com>
2025-03-28 07:45:58 +0700

    well, error

--- backend/src/controllers/schedule.controller.js
+++ backend/src/controllers/schedule.controller.js
@@ -8,7 +8,7 @@ const createSchedule = async (req, res) => {
       areaId = parseInt(areaId) // chuyển từ string sang int
       const scheduleData = req.body;
       const result = await scheduleService.createSchedule(areaId, scheduleData);
-      res.status(StatusCodes.CREATED).json({ success: true, schedule: result });
+      res.status(StatusCodes.CREATED).json(result);
     } catch (error) {
       throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
     }
