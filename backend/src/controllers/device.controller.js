4796f7ad0356781f07df35cc9a8f8965a5d53f42
anhhoang69 <anh.hoangngoc200404@gmail.com>
2025-03-31 09:10:58 +0700

    fix parseInt

--- backend/src/controllers/device.controller.js
+++ backend/src/controllers/device.controller.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import { deviceService } from '../services/device.service.js'
 
 const syncDeviceLogs = async (req, res) => {
-    const { areaId } = req.query
+    const areaId = parseInt(req.query.areaId)
     const logs = await deviceService.syncDeviceLogs(areaId)
     res.status(StatusCodes.OK).json(logs)
 }
